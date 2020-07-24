cd55dda346f014e0d4b655226f9586636c4955cb
ferlyloic <ferlyloic@yahoo.fr>
2020-07-24 12:41:06 +0200

    added firebase and some other dependencies
    added cloud_firestore
    added firebase_auth
    added dio
    added http
    added shared_preferences

---
+++ lib/Authenticate.dart
@@ -0,0 +1,18 @@
+import 'file:///C:/Users/ferly/AndroidStudioProjects/corona_help_app/lib/services/auth_service.dart';
+import 'package:coronahelpapp/main.dart';
+import 'package:flutter/material.dart';
+
+class Authenticate extends StatefulWidget{
+  @override
+  _AuthenticateState createState() => _AuthenticateState();
+}
+
+class _AuthenticateState extends State<Authenticate> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Text('Authenticate'),
+    );
+
+  }
+}
