46999c401c2ad1a1f51d3dbc2ad4e32f50e2f15a
ferlyloic <ferlyloic@yahoo.fr>
2020-07-27 17:07:09 +0200

    StreamProvider for User class

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:coronahelpapp/models/user.dart';
 import 'package:coronahelpapp/screens/BottomNavigationBar.dart';
 import 'package:coronahelpapp/services/auth_service.dart';
 import 'package:flutter/material.dart';
@@ -11,7 +12,7 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return StreamProvider.value(
+    return StreamProvider<User>.value(
         value: AuthService().user,
         child: MaterialApp(
           title: 'Corona Help',
