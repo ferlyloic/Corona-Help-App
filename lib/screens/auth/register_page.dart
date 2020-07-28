985103eca61719502bec077f134238a9d89be889
ferlyloic <ferlyloic@yahoo.fr>
2020-07-28 17:05:43 +0200

    Create register_page.dart

---
+++ lib/screens/auth/register_page.dart
@@ -0,0 +1,94 @@
+import 'package:coronahelpapp/main.dart';
+import 'package:flutter/material.dart';
+
+class RegisterView extends StatefulWidget{
+  @override
+  RegisterViewState createState() => RegisterViewState();
+
+}
+
+class RegisterViewState extends State<RegisterView>{
+  final _formKey = GlobalKey<FormState>();
+  String _username;
+  String _password;
+  String _email;
+
+  bool _autovalidate = false;
+
+  @override
+  Widget build(BuildContext context) {
+    print("building LoginView");
+    return Scaffold(
+        body:Container(
+      padding: EdgeInsets.all(20.0),
+      child: Form(
+        key: _formKey,
+        autovalidate: _autovalidate,
+        child: Column(
+          children: <Widget>[
+            SizedBox(height: 20.0),
+            Text(
+              'Register Information',
+              style: TextStyle(fontSize: 20, color: MyApp.defaultPrimaryColor),
+            ),
+            SizedBox(height: 20.0),
+            TextFormField(
+                onChanged: (value)  {
+                  _username = value;
+//                  print(value);
+                },
+                onSaved: (value)  {
+                  _username = value;
+//                  print(value);
+                },
+                keyboardType: TextInputType.text,
+                validator: (String arg) {
+                  if(arg.length < 4)
+                    return 'Name must be more than 4 charater';
+                  else
+                    return null;
+                },
+                decoration: InputDecoration(labelText: "User name")),
+            TextFormField(
+                onChanged: (value)  {
+                  _email = value;
+//                  print(value);
+                },
+                keyboardType: TextInputType.emailAddress,
+                decoration: InputDecoration(labelText: "Email Address")),
+            TextFormField(
+                onChanged: (value) => _password = value,
+                obscureText: true,
+                decoration: InputDecoration(labelText: "Password")),
+            TextFormField(
+                onChanged: (value) => _password = value,
+                obscureText: true,
+                decoration: InputDecoration(labelText: "Repeat password")),
+            RaisedButton(
+                color: MyApp.defaultPrimaryColor,
+                child: Text(
+                  "Register",
+                  style: TextStyle(
+                      color:
+                      MyApp.isDark(context) ? Colors.black : Colors.white),
+                ),
+                onPressed: () async {
+                  print("register pressed");
+                  print(_email);
+                  print(_password);
+
+//                  User user = await _authService.anonymSign();
+//                  if (user != null) {
+//                    print("Result: ${user.uid}");
+//                  } else {
+////                    TODO: implement an error response here.
+//                    print("Result: no user returned");
+//                  }
+                }),
+          ],
+        ),
+      ),
+    )
+    );
+  }
+}
