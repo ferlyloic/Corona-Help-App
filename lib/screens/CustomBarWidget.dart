8b1564c30e1f8e431540d17c05aced037fd2658f
ferlyloic <ferlyloic@yahoo.fr>
2020-07-29 23:05:03 +0200

    Update CustomBarWidget.dart

--- lib/screens/CustomBarWidget.dart
+++ lib/screens/CustomBarWidget.dart
@@ -8,91 +8,74 @@ class CustomBarWidget extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
+    return Container(
       key: _scaffoldKey,
-      body: Container(
-        height: 160.0,
-        child: Stack(
+      child: Column(
+//        height: 160.0,
           children: <Widget>[
-//            Container(
-//              color: MyApp.getModeColor(context),
-//              width: MediaQuery.of(context).size.width,
-//              height: 160.0,
-//              child: Center(
-//                child: Text(
-//                  HomePage.titleString,
-//                  style: TextStyle(
-//                      color: MyApp.getTitleColor(context), fontSize: 22.0),
-//                ),
-//              ),
-//            ),
-            Positioned(
-              top: 10.0,
-              left: 0.0,
-              right: 0.0,
-              child: Container(
-                padding: EdgeInsets.symmetric(horizontal: 20.0),
-                child: DecoratedBox(
-                  decoration: BoxDecoration(
-                      borderRadius: BorderRadius.circular(1.0),
-                      border: Border.all(
-                          color: Colors.grey.withOpacity(0.5), width: 1.0),
-                      color: MyApp.isDark(context)
-                          ? Colors.black87
-                          : Colors.white),
-                  child: Row(
-                    children: [
-                      IconButton(
-                        icon: Icon(
-                          Icons.menu,
-                          color: MyApp.defaultPrimaryColor,
-                        ),
-                        onPressed: () {
-                          print("your menu action here");
-                          _scaffoldKey.currentState.openDrawer();
-                        },
+            
+          SizedBox(height: 10,),
+            Container(
+              padding: EdgeInsets.symmetric(horizontal: 20.0),
+              child: DecoratedBox(
+                decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(1.0),
+                    border: Border.all(
+                        color: Colors.grey.withOpacity(0.5), width: 1.0),
+                    color: MyApp.isDark(context)
+                        ? Colors.black87
+                        : Colors.white),
+                child: Row(
+                  children: [
+                    IconButton(
+                      icon: Icon(
+                        Icons.menu,
+                        color: MyApp.defaultPrimaryColor,
                       ),
-                      Expanded(
-                        child: TextField(
-                          cursorColor: Colors.black,
+                      onPressed: () {
+                        print("your menu action here");
+                        _scaffoldKey.currentState.openDrawer();
+                      },
+                    ),
+                    Expanded(
+                      child: TextField(
+                        cursorColor: Colors.black,
 //                          keyboardType: inputType,
-                          decoration: new InputDecoration(
-                            border: InputBorder.none,
-                            focusedBorder: InputBorder.none,
-                            enabledBorder: InputBorder.none,
-                            errorBorder: InputBorder.none,
-                            disabledBorder: InputBorder.none,
-                            contentPadding: EdgeInsets.only(
-                                left: 15, bottom: 11, top: 11, right: 15),
-                            hintText: "Search",
-                          ),
+                        decoration: new InputDecoration(
+                          border: InputBorder.none,
+                          focusedBorder: InputBorder.none,
+                          enabledBorder: InputBorder.none,
+                          errorBorder: InputBorder.none,
+                          disabledBorder: InputBorder.none,
+                          contentPadding: EdgeInsets.only(
+                              left: 15, bottom: 11, top: 11, right: 15),
+                          hintText: "Search",
                         ),
                       ),
-                      IconButton(
-                        icon: Icon(
-                          Icons.search,
-                          color: MyApp.defaultPrimaryColor,
-                        ),
-                        onPressed: () {
-                          print("your menu action here");
-                        },
+                    ),
+                    IconButton(
+                      icon: Icon(
+                        Icons.search,
+                        color: MyApp.defaultPrimaryColor,
                       ),
-                      IconButton(
-                        icon: Icon(
-                          Icons.notifications,
-                          color: MyApp.defaultPrimaryColor,
-                        ),
-                        onPressed: () {
-                          print("your menu action here");
-                        },
+                      onPressed: () {
+                        print("your menu action here");
+                      },
+                    ),
+                    IconButton(
+                      icon: Icon(
+                        Icons.notifications,
+                        color: MyApp.defaultPrimaryColor,
                       ),
-                    ],
-                  ),
+                      onPressed: () {
+                        print("your menu action here");
+                      },
+                    ),
+                  ],
                 ),
               ),
-            )
+            ),
           ],
-        ),
       ),
     );
   }
