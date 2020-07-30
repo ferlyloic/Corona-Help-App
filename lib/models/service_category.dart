ab9dd21189b7dc0dade8201a21dbd9cc49a701c1
ferlyloic <ferlyloic@yahoo.fr>
2020-07-30 20:17:01 +0200

    Update service_category.dart

--- lib/models/service_category.dart
+++ lib/models/service_category.dart
@@ -1,1 +1,16 @@
-class ServiceCategory {}
+class ServiceCategory {
+  final String name;
+  final String description;
+  static List<ServiceCategory> _categoryList = [
+    ServiceCategory('Test 01'),
+    ServiceCategory('Test 02'),
+    ServiceCategory('Test 03'),
+    ServiceCategory('Test 04'),
+    ServiceCategory('Test 05'),
+  ];
+
+  ServiceCategory(this.name, {this.description = ''});
+  static all(){
+    return _categoryList;
+  }
+}
