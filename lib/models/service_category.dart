ff0a8b531380e400c137620971157a40c0b2ae97
ferlyloic <ferlyloic@yahoo.fr>
2020-07-31 16:07:33 +0200

    Update service_category.dart

--- lib/models/service_category.dart
+++ lib/models/service_category.dart
@@ -2,11 +2,16 @@ class ServiceCategory {
   final String name;
   final String description;
   static List<ServiceCategory> _categoryList = [
-    ServiceCategory('Test 01'),
-    ServiceCategory('Test 02'),
-    ServiceCategory('Test 03'),
-    ServiceCategory('Test 04'),
-    ServiceCategory('Test 05'),
+    ServiceCategory('Kranken- und Altenbesuch'),
+    ServiceCategory('Unterstützung beim Einkauf'),
+    ServiceCategory('Begleitung zu Bank, Arzt, Behörden'),
+    ServiceCategory('Hilfe bei Telefonaten'),
+    ServiceCategory('Vorlesen / Gespräche / Zuhören'),
+    ServiceCategory('Kleinere Reparaturen im Haushalt'),
+    ServiceCategory('Spaziergänge / Ausflüge'),
+    ServiceCategory('Kurzfristige Kinderbetreuung'),
+    ServiceCategory('Hilfe beim Ausfüllen von Formularen'),
+    ServiceCategory('Begleitung zu einem Gottesdienst'),
   ];
 
   ServiceCategory(this.name, {this.description = ''});
