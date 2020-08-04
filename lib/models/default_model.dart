f46c89f1cda0a1222da481b09e2456282fbd405a
ferlyloic <ferlyloic@yahoo.fr>
2020-08-05 01:03:15 +0200

    added database_service, default_model and create_offer_view

---
+++ lib/models/default_model.dart
@@ -0,0 +1,9 @@
+abstract class DefaultModel{
+
+  Map<String,dynamic > toMap();
+  bool save(){
+    // TODO: implement save
+    throw UnimplementedError();
+  }
+
+}
---
+++ lib/services/database_service.dart
@@ -0,0 +1,11 @@
+
+import 'package:cloud_firestore/cloud_firestore.dart';
+
+class DatabaseService {
+
+  /// user collection reference
+  final CollectionReference userCollection = Firestore.instance.collection('users');
+  final CollectionReference serviceCategoriesCollection = Firestore.instance.collection('service_categories');
+  final CollectionReference servicesCollection = Firestore.instance.collection('services');
+  final CollectionReference serviceStatusesCollection = Firestore.instance.collection('service_statuses');
+}
