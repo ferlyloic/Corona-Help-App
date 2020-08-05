02d3de5729423217de520fb2546dc18d899f708b
ferlyloic <ferlyloic@yahoo.fr>
2020-08-05 18:08:16 +0200

    added deleteUser method

--- lib/services/auth_service.dart
+++ lib/services/auth_service.dart
@@ -1,5 +1,6 @@
 import 'dart:async';
 import 'package:coronahelpapp/models/user.dart';
+import 'package:coronahelpapp/services/database_service.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
@@ -10,7 +11,7 @@ class AuthService {
 
   /// return User from firebase user object [firebaseUser].
   User _userFromFirebaseUser(FirebaseUser firebaseUser) {
-    _currentUser = firebaseUser != null ? User(firebaseUser.uid) : null;
+    _currentUser = firebaseUser != null ? User(firebaseUser) : null;
     return _currentUser;
   }
 
@@ -67,5 +68,33 @@ class AuthService {
       print(result.toString());
       FirebaseUser firebaseUser = result.user;
       return _userFromFirebaseUser(firebaseUser);
+  }
+//  /// delete the user with corresponding [email] and [password].
+//  Future deleteUser(String email, String password) async {
+//    try {
+//      FirebaseUser user = await _auth.currentUser();
+//      AuthCredential credentials =
+//      EmailAuthProvider.getCredential(email: email, password: password);
+//      print(user);
+//      AuthResult result = await user.reauthenticateWithCredential(credentials);
+//      await DatabaseService(uid: result.user.uid).deleteUser(); // called from database class
+//      await result.user.delete();
+//      return true;
+//    } catch (e) {
+//      print(e.toString());
+//      return null;
+//    }
+//  }
+  /// delete the current user in [context].
+  Future deleteUser(BuildContext context) async {
+    try {
+    User user = AuthService().getCurrentUser(context);
+      await DatabaseService(uid: user.userFromFirebase.uid).deleteUser(); // called from database class
+      await user.userFromFirebase.delete();
+      return true;
+    } catch (e) {
+      print(e.toString());
+      return null;
+    }
   }
 }
