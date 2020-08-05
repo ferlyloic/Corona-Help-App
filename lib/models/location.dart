52810c3a807d435281a5a8089a675d50f79d7077
ferlyloic <ferlyloic@yahoo.fr>
2020-08-05 18:00:47 +0200

    Update location.dart
    added getCurrentLocation() method

--- lib/models/location.dart
+++ lib/models/location.dart
@@ -1,5 +1,12 @@
 
+import 'package:geolocator/geolocator.dart';
+
 class Location {
+  final Geolocator geolocator = Geolocator()..forceAndroidLocationManager;
+  Position _currentPosition;
+  String _currentAddress;
+
+
   String houseNumber = 'N/A';
   String street = 'N/A';
   String zip  = 'N/A';
@@ -16,5 +23,31 @@ class Location {
     if(this.country != null) result += this.country;
     return result;
   }
+  getCurrentLocation() {
+    geolocator
+        .getCurrentPosition(desiredAccuracy: LocationAccuracy.low)
+        .then((Position position) {
+      _currentPosition = position;
+      _getAddressFromLatLng();
+    }).catchError((e) {
+      print(e);
+    });
+  }
 
+  _getAddressFromLatLng() async {
+    try {
+      List<Placemark> p = await geolocator.placemarkFromCoordinates(
+          _currentPosition.latitude, _currentPosition.longitude);
+
+      Placemark place = p[0];
+      print(place.toJson());
+      this.houseNumber = place.subThoroughfare;
+      this.houseNumber = place.thoroughfare;
+      this.zip = place.postalCode;
+      this.city = place.locality;
+      this.country = place.country;
+    } catch (e) {
+      print(e);
+    }
+  }
 }
