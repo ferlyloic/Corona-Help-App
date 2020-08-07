4d8e58d561197edb67926ed14a68b0820988ca15
ferlyloic <ferlyloic@yahoo.fr>
2020-08-07 13:14:19 +0200

    Update main.dart
    added StreamProvider<QuerySnapshot>

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:coronahelpapp/models/user.dart';
 import 'package:coronahelpapp/screens/BottomNavigationBar.dart';
 import 'package:coronahelpapp/services/auth_service.dart';
@@ -8,24 +9,30 @@ void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
   static Color defaultPrimaryColor = Colors.orange;
+  static const kGoogleApiKey = "AIzaSyBQCnZmVPPtw6qhnhB6w9IKa99zNDrydxA";
+//  GoogleMapsPlaces _places = GoogleMapsPlaces(apiKey: kGoogleApiKey);
+  static const String apiQueryLanguage = 'de';
 
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return StreamProvider<User>.value(
-        value: AuthService().user,
-        child: MaterialApp(
-          title: 'Corona Help App',
-          theme: ThemeData(
-            primarySwatch: defaultPrimaryColor,
-            accentColor: defaultPrimaryColor,
+        value: User(null).user,
+        child: StreamProvider<QuerySnapshot>.value(
+          value: User(null).dataFromFireStore,
+          child: MaterialApp(
+            title: 'Corona Help App',
+            theme: ThemeData(
+              primarySwatch: defaultPrimaryColor,
+              accentColor: defaultPrimaryColor,
+            ),
+            darkTheme: ThemeData(
+              brightness: Brightness.dark,
+              primarySwatch: defaultPrimaryColor,
+              accentColor: defaultPrimaryColor,
+            ),
+            home: MyBottomNavigationBar(),
           ),
-          darkTheme: ThemeData(
-            brightness: Brightness.dark,
-            primarySwatch: defaultPrimaryColor,
-            accentColor: defaultPrimaryColor,
-          ),
-          home: MyBottomNavigationBar(),
         ));
   }
 
