4686449770f27bdb28d76cb63a09c1953444e147
ferlyloic <ferlyloic@yahoo.fr>
2020-08-07 19:35:59 +0200

    Update service.dart
    added class Service

--- lib/models/service.dart
+++ lib/models/service.dart
@@ -1,1 +1,37 @@
 
+import 'package:coronahelpapp/models/default_model.dart';
+import 'package:coronahelpapp/models/service_category.dart';
+import 'package:coronahelpapp/models/service_status.dart';
+import 'package:coronahelpapp/models/user.dart';
+
+class Service extends DefaultModel{
+  String id;
+  User helpProvider;
+  User helpReceiver;
+  ServiceCategory category;
+  ServiceStatus status;
+  String description;
+  DateTime createdAt;
+
+  @override
+  // TODO: implement collectionName
+  String get collectionName => 'services';
+
+  @override
+  // TODO: implement data
+  Map<String, dynamic> get data {
+   return {
+     'help_provider':this.helpProvider?.toString(),
+     'help_receiver':this.helpReceiver?.toString(),
+     'category':this.category?.name,
+     'description':this.description,
+     'status':this.status?.toString(),
+   };
+  }
+
+  @override
+  Map<String, dynamic> toMap() {
+    // TODO: implement toMap
+    throw UnimplementedError();
+  }
+}
