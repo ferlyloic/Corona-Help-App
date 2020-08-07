0f72d85a136347d6258f4253d7f0e234788fb2d2
ferlyloic <ferlyloic@yahoo.fr>
2020-08-07 13:22:58 +0200

    Update location.dart
    name refactor to LocationInfo
    update getCurrentLocation(), _getAddressFromLatLng()
    add toJson(), fromJson(data)

--- lib/models/location.dart
+++ lib/models/location.dart
@@ -1,7 +1,7 @@
 
 import 'package:geolocator/geolocator.dart';
 
-class Location {
+class LocationInfo {
   final Geolocator geolocator = Geolocator()..forceAndroidLocationManager;
   Position _currentPosition;
   String _currentAddress;
@@ -9,45 +9,67 @@ class Location {
 
   String houseNumber = 'N/A';
   String street = 'N/A';
-  String zip  = 'N/A';
+  String postalCode  = 'N/A';
   String city = 'N/A';
   String country  = 'N/A';
-  Location();
+  LocationInfo();
   @override
   String toString() {
     String result = '';
     if(this.street != null) result += this.street + ' ';
     if(this.houseNumber != null) result += this.houseNumber + ', ';
-    if(this.zip != null) result += this.zip + ' ';
+    if(this.postalCode != null) result += this.postalCode + ' ';
     if(this.city != null) result += this.city + ', ';
     if(this.country != null) result += this.country;
     return result;
   }
-  getCurrentLocation() {
-    geolocator
+   getCurrentLocation() async {
+    Placemark p = await geolocator
         .getCurrentPosition(desiredAccuracy: LocationAccuracy.low)
         .then((Position position) {
       _currentPosition = position;
-      _getAddressFromLatLng();
+      return _getAddressFromLatLng();
+
     }).catchError((e) {
       print(e);
     });
+    return p;
   }
 
-  _getAddressFromLatLng() async {
+  Future<Placemark> _getAddressFromLatLng() async {
     try {
       List<Placemark> p = await geolocator.placemarkFromCoordinates(
           _currentPosition.latitude, _currentPosition.longitude);
-
       Placemark place = p[0];
       print(place.toJson());
       this.houseNumber = place.subThoroughfare;
-      this.houseNumber = place.thoroughfare;
-      this.zip = place.postalCode;
+      this.street = place.thoroughfare;
+      this.postalCode = place.postalCode;
       this.city = place.locality;
       this.country = place.country;
+      print(this);
+      return p[0];
     } catch (e) {
       print(e);
+      return null;
     }
+  }
+
+  Map<String, String> toJson() {
+    return {
+      'house_number': this.houseNumber,
+      'street': this.street,
+      'postal_code': this.postalCode,
+      'city': this.city,
+      'country': this.country,
+    };
+  }
+
+  void fromJson(data) {
+    this.houseNumber = data['house_number'];
+    this.street = data['street'];
+    this.postalCode = data['postal_code'];
+    this.city = data['city'];
+    this.country = data['country'];
   }
 }
