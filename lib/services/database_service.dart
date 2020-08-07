68a31d075c985e4cab26cdf62d59d966e6407ed9
ferlyloic <ferlyloic@yahoo.fr>
2020-08-07 13:51:15 +0200

    Update database_service.dart

--- lib/services/database_service.dart
+++ lib/services/database_service.dart
@@ -8,4 +8,15 @@ class DatabaseService {
   final CollectionReference serviceCategoriesCollection = Firestore.instance.collection('service_categories');
   final CollectionReference servicesCollection = Firestore.instance.collection('services');
   final CollectionReference serviceStatusesCollection = Firestore.instance.collection('service_statuses');
+  final String uid;
+
+  DatabaseService({this.uid});
+
+  Future updateUserData() async {
+    return await userCollection.document(uid).delete();
+  }
+
+  Future deleteUser() {
+    return userCollection.document(uid).delete();
+  }
 }
