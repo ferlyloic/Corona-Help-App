7d012a3857eb5b4ba9b6b01210578d0caa3233e0
ferlyloic <ferlyloic@yahoo.fr>
2020-08-07 13:20:17 +0200

    Update default_model.dart
    added abstract get id, get CollectionName, get data
    added create method
    added get dataFromFireStore

--- lib/models/default_model.dart
+++ lib/models/default_model.dart
@@ -1,9 +1,33 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+
 abstract class DefaultModel{
+  CollectionReference collection;
+
+
+  String get collectionName;
+  String get id;
+DefaultModel(){
+  collection = Firestore.instance.collection(collectionName);
+}
+  Map<String, dynamic> get data;
+
+
 
   Map<String,dynamic > toMap();
   bool save(){
-    // TODO: implement save
     throw UnimplementedError();
   }
+//  Future updateUserData() async {
+//    return await collection.document(id).updateData(data);
+//  }
+  create() async {
+//    Map<String, dynamic> dataWithCreatedDateTine = {'crated_at': DateTime.now()};
+    data['created_at'] = DateTime.now().toString();
+    print(data);
+    return await collection.document(id).setData(data, merge: true);
+  }
+Stream <QuerySnapshot> get dataFromFireStore {
+    return collection.snapshots();
+}
 
 }
