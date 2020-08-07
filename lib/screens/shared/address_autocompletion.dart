808eca1ed024fd9797c2624a168dab0d2b40a4af
ferlyloic <ferlyloic@yahoo.fr>
2020-08-07 13:26:13 +0200

    Create address_autocompletion.dart
    example for address autocompletion
    (to be removed later)

---
+++ lib/screens/shared/address_autocompletion.dart
@@ -0,0 +1,205 @@
+import 'dart:async';
+
+import 'package:coronahelpapp/main.dart';
+import 'package:google_maps_webservice/places.dart';
+import 'package:flutter_google_places/flutter_google_places.dart';
+import 'package:flutter/material.dart';
+import 'dart:math';
+
+// to get places detail (lat/lng)
+GoogleMapsPlaces _places = GoogleMapsPlaces(apiKey: MyApp.kGoogleApiKey);
+
+//
+//main() {
+//  runApp(RoutesWidget());
+//}
+//
+//final customTheme = ThemeData(
+//  primarySwatch: Colors.blue,
+//  brightness: Brightness.dark,
+//  accentColor: Colors.redAccent,
+//  inputDecorationTheme: InputDecorationTheme(
+//    border: OutlineInputBorder(
+//      borderRadius: BorderRadius.all(Radius.circular(4.00)),
+//    ),
+//    contentPadding: EdgeInsets.symmetric(
+//      vertical: 12.50,
+//      horizontal: 10.00,
+//    ),
+//  ),
+//);
+//
+//class RoutesWidget extends StatelessWidget {
+//  @override
+//  Widget build(BuildContext context) => MaterialApp(
+//    title: "My App",
+//    theme: customTheme,
+//    routes: {
+//      "/": (_) => AddressAutocompletion(),
+//      "/search": (_) => CustomSearchScaffold(),
+//    },
+//  );
+//}
+
+class AddressAutocompletion extends StatefulWidget {
+  @override
+  _AddressAutocompletionState createState() => _AddressAutocompletionState();
+}
+
+final homeScaffoldKey = GlobalKey<ScaffoldState>();
+final searchScaffoldKey = GlobalKey<ScaffoldState>();
+
+class _AddressAutocompletionState extends State<AddressAutocompletion> {
+  Mode _mode = Mode.overlay;
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              _buildDropdownMenu(),
+              RaisedButton(
+                onPressed: _handlePressButton,
+                child: Text("Search places"),
+              ),
+              RaisedButton(
+                child: Text("Custom"),
+                onPressed: () {
+                  Navigator.of(context).pushNamed("/search");
+                },
+              ),
+            ],
+          ),
+    );
+  }
+
+  Widget _buildDropdownMenu() => DropdownButton(
+    value: _mode,
+    items: <DropdownMenuItem<Mode>>[
+      DropdownMenuItem<Mode>(
+        child: Text("Overlay"),
+        value: Mode.overlay,
+      ),
+      DropdownMenuItem<Mode>(
+        child: Text("Fullscreen"),
+        value: Mode.fullscreen,
+      ),
+    ],
+    onChanged: (m) {
+      setState(() {
+        _mode = m;
+      });
+    },
+  );
+
+  void onError(PlacesAutocompleteResponse response) {
+//    print(response.errorMessage);
+//    return;
+    homeScaffoldKey.currentState.showSnackBar(
+      SnackBar(content: Text(response.errorMessage)),
+    );
+  }
+
+  Future<void> _handlePressButton() async {
+    // show input autocomplete with selected mode
+    // then get the Prediction selected
+    Prediction p = await PlacesAutocomplete.show(
+      context: context,
+      apiKey: MyApp.kGoogleApiKey,
+      onError: onError,
+      mode: _mode,
+      language: MyApp.apiQueryLanguage,
+      components: [Component(Component.country, MyApp.apiQueryLanguage)],
+    );
+
+    displayPrediction(p, homeScaffoldKey.currentState);
+  }
+}
+
+Future<Null> displayPrediction(Prediction p, ScaffoldState scaffold) async {
+  if (p != null) {
+    // get detail (lat/lng)
+    PlacesDetailsResponse detail = await _places.getDetailsByPlaceId(p.placeId);
+    final lat = detail.result.geometry.location.lat;
+    final lng = detail.result.geometry.location.lng;
+    print(p);
+    scaffold.showSnackBar(
+      SnackBar(content: Text("${p.description} - $lat/$lng")),
+    );
+  }
+}
+
+// custom scaffold that handle search
+// basically your widget need to extends [GooglePlacesAutocompleteWidget]
+// and your state [GooglePlacesAutocompleteState]
+class CustomSearchScaffold extends PlacesAutocompleteWidget {
+  CustomSearchScaffold()
+      : super(
+    apiKey: MyApp.kGoogleApiKey,
+    sessionToken: Uuid().generateV4(),
+    language: MyApp.apiQueryLanguage,
+    components: [Component(Component.country, MyApp.apiQueryLanguage)],
+  );
+
+  @override
+  _CustomSearchScaffoldState createState() => _CustomSearchScaffoldState();
+}
+
+class _CustomSearchScaffoldState extends PlacesAutocompleteState {
+  @override
+  Widget build(BuildContext context) {
+    final appBar = AppBar(title: AppBarPlacesAutoCompleteTextField());
+    final body = PlacesAutocompleteResult(
+      onTap: (p) {
+        displayPrediction(p, searchScaffoldKey.currentState);
+      },
+      logo: Row(
+        children: [FlutterLogo()],
+        mainAxisAlignment: MainAxisAlignment.center,
+      ),
+    );
+    return Scaffold(key: searchScaffoldKey, appBar: appBar, body: body);
+  }
+
+  @override
+  void onResponseError(PlacesAutocompleteResponse response) {
+    super.onResponseError(response);
+    searchScaffoldKey.currentState.showSnackBar(
+      SnackBar(content: Text(response.errorMessage)),
+    );
+  }
+
+  @override
+  void onResponse(PlacesAutocompleteResponse response) {
+    super.onResponse(response);
+    if (response != null && response.predictions.isNotEmpty) {
+      searchScaffoldKey.currentState.showSnackBar(
+        SnackBar(content: Text("Got answer")),
+      );
+    }
+  }
+}
+
+class Uuid {
+  final Random _random = Random();
+
+  String generateV4() {
+    // Generate xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx / 8-4-4-4-12.
+    final int special = 8 + _random.nextInt(4);
+
+    return '${_bitsDigits(16, 4)}${_bitsDigits(16, 4)}-'
+        '${_bitsDigits(16, 4)}-'
+        '4${_bitsDigits(12, 3)}-'
+        '${_printDigits(special, 1)}${_bitsDigits(12, 3)}-'
+        '${_bitsDigits(16, 4)}${_bitsDigits(16, 4)}${_bitsDigits(16, 4)}';
+  }
+
+  String _bitsDigits(int bitCount, int digitCount) =>
+      _printDigits(_generateBits(bitCount), digitCount);
+
+  int _generateBits(int bitCount) => _random.nextInt(1 << bitCount);
+
+  String _printDigits(int value, int count) =>
+      value.toRadixString(16).padLeft(count, '0');
+}
