aa729b146db753fb909cf7fe3da062f62b5a02da
ferlyloic <ferlyloic@yahoo.fr>
2020-08-07 19:24:30 +0200

    Update OfferPage.dart
    added category dropdown menu
    added desscription input field

--- lib/screens/OfferPage.dart
+++ lib/screens/OfferPage.dart
@@ -1,12 +1,20 @@
 import 'package:coronahelpapp/main.dart';
+import 'package:coronahelpapp/models/service.dart';
 import 'package:coronahelpapp/models/service_category.dart';
+import 'package:coronahelpapp/models/service_status.dart';
+import 'package:coronahelpapp/models/user.dart';
+import 'package:coronahelpapp/models/user_role.dart';
 import 'package:coronahelpapp/screens/offer/categories_list_view.dart';
 import 'package:coronahelpapp/screens/offer/create_offer_view.dart';
+import 'package:coronahelpapp/screens/shared/loading.dart';
+import 'package:coronahelpapp/services/auth_service.dart';
+import 'package:coronahelpapp/services/validation_service.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_multiselect/flutter_multiselect.dart';
 
 class OfferPage extends StatefulWidget {
   OfferPage({Key key}) : super(key: key);
-  static const String titleString = "Offer";
+  static const String titleString = "Beantragen";
 
   @override
   State<StatefulWidget> createState() {
@@ -15,37 +23,157 @@ class OfferPage extends StatefulWidget {
 }
 
 class _OfferPageState extends State<OfferPage> {
+  final _formKey = GlobalKey<FormState>();
   ServiceCategory chosenCategory;
-  String textOverCategories ='Wähle eine Kategorie aus';
+  String textOverCategories = 'Wähle eine Kategorie aus';
+
+  bool _onLoading = false;
+
+  String _description;
+  ServiceCategory _category;
+  String _errorMessage;
   @override
   Widget build(BuildContext context) {
 
+    User user = AuthService().getCurrentUser(context);
     return Scaffold(
       appBar: AppBar(
         centerTitle: true,
         title: Text(
-          OfferPage.titleString,
+          user == null ? OfferPage.titleString : (user.role == UserRole.HelpProvider ? "Hilfe anbieten" :'Hifle suchen'),
           style: TextStyle(color: MyApp.getTitleColor(context)),
         ),
       ),
-      body: chosenCategory!= null ? CreateOfferView(setChosenCategory: setChosenCategory,category: chosenCategory,): SingleChildScrollView(
-    child: Center(
-    child: Column(
-        children: [
-          SizedBox(height: 20.0),
-          Text(
-            this.textOverCategories,
-            style: TextStyle(fontSize: 20, color: MyApp.defaultPrimaryColor),
-          ),
-          SizedBox(height: 20.0),
-          CategoriesListWidget(setChosenCategory: setChosenCategory),
-        ],
-      ),
-    )
-    )
+      body: _onLoading
+          ? Loading()
+          : SingleChildScrollView(
+              child: Container(
+                padding: EdgeInsets.all(20.0),
+                child: Form(
+                    key: _formKey,
+                    child: Column(children: <Widget>[Text(
+                      'Formular ausfüllen',
+                      style: TextStyle(
+                          fontSize: 20, color: MyApp.defaultPrimaryColor),
+                    ),
+                      SizedBox(height: 20.0),
+                      DropdownButtonFormField<ServiceCategory>(
+                        validator: (value) =>
+                            value == null ? 'field required' : null,
+                        hint: Text(
+                          'Kategorie auswählen',
+                          style: TextStyle(
+                            color: Colors.grey,
+                          ),
+                        ),
+                        value: _category,
+                        icon: Icon(Icons.arrow_drop_down),
+                        iconSize: 24,
+                        elevation: 16,
+                        style: TextStyle(
+//                  color: Colors.deepPurple
+                            ),
+                        onChanged: (ServiceCategory newValue) {
+                          setState(() {
+                            _category = newValue;
+                          });
+                          print(_category);
+                        },
+                        items: ServiceCategory.all(context)
+                            .map<DropdownMenuItem<ServiceCategory>>(
+                                (ServiceCategory value) {
+                          return DropdownMenuItem<ServiceCategory>(
+                            value: value,
+                            child: Text(
+                               value.name,
+                              style: TextStyle(
+                                color: MyApp.defaultPrimaryColor,
+                              ),
+
+                            ),
+                          );
+                        }).toList(),
+                      ),
+                      SizedBox(height: 20.0),
+
+                      TextFormField(
+                          onChanged: (value) {
+                            _description = value;
+                          },
+                          keyboardType: TextInputType.multiline,
+                          minLines: 1,//Normal textInputField will be displayed
+                          maxLines: 5,// when user presses enter it will adapt to it
+                          validator: (String arg) {
+                            ValidationService val = ValidationService(arg);
+                            val.minLength(15);
+                            val.minWordCharacterNumber(1);
+//                  val.hasNoWhiteSpaces();
+                            return val.errorResult();
+                          },
+                          decoration: InputDecoration(labelText: "Description")),
+                      _getErrorTextWidget(),
+                      SizedBox(height: 20,),
+                      RaisedButton(
+                          color: MyApp.defaultPrimaryColor,
+                          child: Text(
+                            "Antrag speichern",
+                            style: TextStyle(
+                                color:
+                                MyApp.isDark(context) ? Colors.black : Colors.white),
+                          ),
+
+                          onPressed: () async {
+                            print("register pressed");
+                            if(_formKey.currentState.validate()) {
+                              setState(() {
+                                _onLoading = true;
+                              });
+
+                              print(_category);
+                              print(_description);
+//                              try{
+                                Service service = Service();
+                                service.category = _category;
+                                if(user.role == UserRole.HelpProvider) {
+                                  service.helpProvider = user;
+                                }else{
+                                  service.helpReceiver = user;
+                                }
+                                service.description = _description;
+                                service.status = ServiceStatus.NotStarted;
+                                service.create();
+                                print("Result: ${service}");
+//                              } on /*PlatformException*/Exception catch ( e) {
+//                                setState(() {
+//                                  _onLoading = false;
+//                                  _errorMessage = e.toString();
+//                                  print(_errorMessage);
+//                                });
+//                              } catch ( e) {
+//                                setState(() {
+//                                  _onLoading = false;
+//                                  _errorMessage = 'fails to register. pleas try again later.';
+//                                });
+//                                print(e.toString());
+//                              }
+
+                            }else{
+                              if(_errorMessage != null) setState(() => _errorMessage = null);
+                              print("at least 1 field is not valid");
+                            }
+
+                          }),
+
+                    ])),
+              ),
+            ),
     );
   }
-
+  _getErrorTextWidget() {
+    return _errorMessage == null
+        ? Container()
+        : Stack(children: [Text(_errorMessage,style: TextStyle(color: Colors.red, fontSize: 14.0),), SizedBox(height: 20)]);
+  }
   setChosenCategory(ServiceCategory category) {
     chosenCategory = category;
     setState(() {
