92c90d9971db3050fe3125de73ed4f39889edd11
ferlyloic <ferlyloic@yahoo.fr>
2020-08-07 21:16:28 +0200

    Update HomePage.dart
    some improvements

--- lib/screens/HomePage.dart
+++ lib/screens/HomePage.dart
@@ -1,11 +1,13 @@
+import 'dart:math';
+
 import 'package:coronahelpapp/main.dart';
 import 'package:coronahelpapp/models/service_category.dart';
 import 'package:coronahelpapp/models/user.dart';
 import 'package:coronahelpapp/services/auth_service.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_multiselect/flutter_multiselect.dart';
-
-import 'CustomBarWidget.dart';
+import 'package:http/http.dart' as http;
+import 'dart:convert';
 
 class HomePage extends StatefulWidget {
   HomePage({Key key}) : super(key: key);
@@ -33,24 +35,108 @@ class _HomePageState extends State<HomePage> {
         body: Container(
             padding: EdgeInsets.fromLTRB(10, 10, 10, 0),
             child: Column(children: <Widget>[
-//              CustomBarWidget(),
-//              Container(
-//                child:
-//                    Text(user != null ? user.toString() : "no user logged in"),
-//              ),
-              MultiSelect(
-                  autovalidate: false,
-                  titleText: "Filtern",
-                  validator: (value) {
-                    if (value == null) {
-                      return 'Please select one or more option(s)';
-                    }
-                    return null;
-                  },
-                  errorText: 'Please select one or more option(s)',
-                  dataSource: ServiceCategory.all(context).map((e) {
-                    return {'display':e.name, 'value':e};
-                  }).toList(),
+             _getFilter(),
+              SizedBox(height: 20.0),
+              Expanded(child: _buildList()),
+            ])));
+  }
+
+
+
+
+
+
+  List<dynamic> _users = [];
+  final String apiUrl = "https://randomuser.me/api/?results=100&nat=de";
+
+  Future<void> _getData() async {
+    if (_users != null)
+      setState(() {
+        fetchCategories();
+      });
+  }
+
+  void fetchCategories() async {
+    print(apiUrl);
+    var result = await http.get(apiUrl);
+    setState(() {
+      _users = json.decode(result.body)['results'];
+    });
+  }
+
+  String _name(dynamic user) {
+    return user['name']['title'] +
+        " " +
+        user['name']['first'] +
+        " " +
+        user['name']['last'];
+  }
+
+  String _location(dynamic user) {
+    return user['location']['city'];
+  }
+
+  String _age(Map<dynamic, dynamic> user) {
+    return "Age: " + user['dob']['age'].toString();
+  }
+  Widget _buildList() {
+    return _users?.length != 0
+        ? RefreshIndicator(
+      child: ListView.builder(
+          padding: EdgeInsets.all(8),
+          itemCount: _users.length,
+          itemBuilder: (BuildContext context, int index) {
+            ServiceCategory category = _categorie(context);
+            return Card(
+//              color: MyApp.getModeColor(context),
+              color: category.color(context),
+              child: Column(
+                children: <Widget>[
+                  ListTile(
+                    leading: CircleAvatar(
+                        radius: 30,
+                        backgroundImage: NetworkImage(
+                            _users[index]['picture']['large'])),
+                    title: Text(_name(_users[index])),
+                    subtitle: Text(category.name),
+                    trailing: Text(_location(_users[index])),
+                  )
+                ],
+              ),
+            );
+          }),
+      onRefresh: _getData,
+    )
+        : Center(child: CircularProgressIndicator());
+  }
+  @override
+  void initState() {
+    print("init HOME");
+    super.initState();
+    fetchCategories();
+//    checkLoginStatus();
+  }
+
+  ServiceCategory _categorie(BuildContext context) {
+    var all = ServiceCategory.all(context);
+    return all.elementAt(0 + Random().nextInt(all.length));
+  }
+
+  _getFilter() {
+    return MultiSelect(
+
+        autovalidate: false,
+        titleText: "Filter-Optionen auswählen",
+        validator: (value) {
+          if (value == null) {
+            return 'Please select one or more option(s)';
+          }
+          return null;
+        },
+        errorText: 'Please select one or more option(s)',
+        dataSource: ServiceCategory.all(context).map((e) {
+          return {'display': e.name, 'value': e};
+        }).toList(),
 //                          [
 //                            {
 //                              "display": "Australia",
@@ -69,23 +155,14 @@ class _HomePageState extends State<HomePage> {
 //                              "value": 4,
 //                            }
 //                          ],
-                  textField: 'display',
-                  valueField: 'value',
-                  filterable: true,
+        textField: 'display',
+        valueField: 'value',
+        filterable: true,
+        hintText: 'Keine Option ausgewählt',
 //                  required: true,
-                  value: null,
-                  onSaved: (value) {
-                    print('The value is $value');
-                  }
-              ),
-              SizedBox(height: 20.0),
-            ])));
-  }
-
-  @override
-  void initState() {
-    print("init HOME");
-    super.initState();
-//    checkLoginStatus();
+        value: null,
+        onSaved: (value) {
+          print('The value is $value');
+        });
   }
 }
