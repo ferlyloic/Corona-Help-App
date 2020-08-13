197eedc70de2dfb583f498d4708af1703f164612
ferlyloic <ferlyloic@yahoo.fr>
2020-08-14 00:40:06 +0200

    Update FindPage.dart

--- lib/screens/FindPage.dart
+++ lib/screens/FindPage.dart
@@ -1,10 +1,13 @@
 import 'package:coronahelpapp/main.dart';
 import 'package:coronahelpapp/models/service.dart';
+import 'package:coronahelpapp/models/service_category.dart';
 import 'package:coronahelpapp/models/user.dart';
 import 'package:coronahelpapp/models/user_role.dart';
 import 'package:coronahelpapp/screens/auth/authenticate.dart';
 import 'package:coronahelpapp/services/auth_service.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_html_view/flutter_html_view.dart';
 import 'package:http/http.dart' as http;
 import 'dart:convert';
 
@@ -68,14 +71,17 @@ class _FindPageState extends State<FindPage> {
   Widget _buildList() {
     _user = AuthService().getCurrentUser(context);
     _userServices = Service.all(context);
+
     if(_user != null) {
+      List<User> temp = [];
       print(_userServices);
+//      for(Service s in _userServices){
+//        print(s.helpReceiver);
+//      }
       _userServices = _userServices
           .where((Service element) =>
-      (_user.role == UserRole.HelpProvider
-          ? element.helpProvider
-          : element.helpReceiver) ==
-          _user.userFromFirebase.uid)
+      (element.helpProvider == _user.id) ||
+              (element.helpReceiver == _user.id))
           .toList();
       print(_userServices.length);
     }
@@ -106,20 +112,51 @@ class _FindPageState extends State<FindPage> {
         RefreshIndicator(
       child: ListView.builder(
           padding: EdgeInsets.all(8),
-          itemCount: _usersList.length,
+          itemCount: _userServices.length,
           itemBuilder: (BuildContext context, int index) {
+            ServiceCategory category = ServiceCategory.find(_userServices[index].category, context);
             return Card(
-              color: MyApp.getModeColor(context),
+//              color: MyApp.getModeColor(context),
+              color: category.color(context),
               child: Column(
                 children: <Widget>[
                   ListTile(
-                    leading: CircleAvatar(
-                        radius: 30,
-                        backgroundImage: NetworkImage(
-                            _usersList[index]['picture']['large'])),
-                    title: Text(_name(_usersList[index])),
-                    subtitle: Text(_location(_usersList[index])),
-                    trailing: Text(_age(_usersList[index])),
+//                    leading: CircleAvatar(
+//                        radius: 30,
+//                        backgroundImage: AssetImage('assets/default_user.png')),
+                    title: Text(category != null
+                        ? category.name
+                        : ''
+                    ),
+//                    subtitle: Text(_userServices[index].id),
+                    trailing: Text('seit '+ _getDatetimeDiff(_userServices[index].createdAt)),
+                  ),
+                  Container(
+                    child: Column(
+                      children: [
+                        Text(
+                        _userServices[index].description,
+                      ),
+                        Center(
+                          child: SingleChildScrollView(
+                            child: HtmlView(
+                              data:
+                              """<p>Lorem ipsum sollicitudin velit leo proin vivamus augue dictumst, 
+                              litora commodo netus ut amet auctor eu.</p>\r<p>Habitasse sagittis curabitur 
+                              ligula non convallis scelerisque, purus tristique sed aliquam enim, 
+                              phasellus ut integer lorem posuere.</p>\r<p>Consequat quis mollis vulputate curabitur primis, 
+                              leo platea fusce neque primis, turpis sociosqu hendrerit gravida.</p>\r""",
+                              onLaunchFail: (url) { // optional, type Function
+                                print("launch $url failed");
+                              },
+                              scrollable: false, //false to use MarksownBody and true to use Marksown
+                            ),
+                          ),
+                        ),
+                      ]
+                    ),
+                    padding: EdgeInsets.fromLTRB(20, 0 , 10, 10),
+                    alignment: Alignment.bottomLeft,
                   )
                 ],
               ),
@@ -129,5 +166,12 @@ class _FindPageState extends State<FindPage> {
     )
     :  Container( child:  Text('Sie haben noch keine Einträge'),);
 //    : Center(child: CircularProgressIndicator());
+  }
+
+  String _getDatetimeDiff(DateTime dateTime) {
+    if(dateTime.difference(DateTime.now()).inDays > 0){
+      return dateTime.difference(DateTime.now()).inDays.toString()+' Tag(e)';
+    }
+    return dateTime.hour.toString() + ':'+ dateTime.minute.toString();
   }
 }
